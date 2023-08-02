62cce84b6c5bf2224c8153a742b5a00bc84068cb
SIDVJSINGH <sidvjsingh@gmail.com>
2023-08-02 18:18:22 +0530

    added python

--- terraform_aws_lambda/python_files/python.py
+++ terraform_aws_lambda/python_files/python.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello Terraform !!! LAMBDA here, How you Doing?')
+        'body': json.dumps('Hello Terraform !!! LAMBDA here, How you Doing? for Git Actions')
     }
